687f05b3bbfe03b05e53ac59013e2b041eae6b2b
Emon <emon50666@gmail.com>
2025-01-01 14:14:50 +0600

    database connect in db

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
 const express = require('express');
+const mongoose = require('mongoose')
+
 const app = express();
 app.use(express.json());
 
 const port = 9000
+
+
+// database connect in db
+const connectDB = async()=>{
+    try{
+        await mongoose.connect('mongodb://127.0.0.1:27017/todos',{
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+
+        })
+        console.log('db is connected');
+    }
+    catch(err){
+        console.log(err,'db not connect');
+    }
+}
+
+
 
 
 app.get('/',(req,res)=>{
@@ -11,4 +31,5 @@ app.get('/',(req,res)=>{
 
 app.listen(port,async()=>{
     console.log(`server running this ${port} port`);
+    await connectDB()
 })
